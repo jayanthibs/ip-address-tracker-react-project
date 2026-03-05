57e3dc5391632ad5ec548b4b4466a9ac211dcfea
Jayanthi Balasubramani <jayanthibs@gmail.com>
2026-03-05 17:40:06 -0500

    added custom Icon for Marker

--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -1,35 +1,30 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import L from "leaflet";
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-// Fix default marker icon issue in React
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIcon2x,
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow,
-});
+import Mark from "../assets/images/icon-location.svg";
 
-function MyMap({data}) {
-      const lat = data?.location?.lat ?? 40.7128;
-  const lng = data?.location?.lng ?? -74.0060;
+function MyMap({ data }) {
+  const lat = data?.location?.lat ?? 40.7128;
+  const lng = data?.location?.lng ?? -74.006;
+
+  const customIcon = L.icon({
+    iconUrl: Mark,
+    iconSize: [35, 45],
+    iconAnchor: [17, 45],
+  });
 
   return (
     <MapContainer
       center={[lat, lng]}
       zoom={13}
       className="h-[500px] w-full z-0"
-    //   style={{ height: "500px", width: "100%" }}
+      //   style={{ height: "500px", width: "100%" }}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; OpenStreetMap contributors"
       />
-      <Marker position={[lat, lng]}>
-        {/* <Popup>Hello from React Leaflet!</Popup> */}
-      </Marker>
+      <Marker position={[lat, lng]} icon={customIcon} />
     </MapContainer>
   );
 }
